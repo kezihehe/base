b78e6cdf36ad336df0ae720c9938e2e7d91ab6c9
lisk <shengke.li@ds365.com>
2016-07-12 20:10:00 +0800

    调用service

--- ssm-base/src/main/java/com/lisk/demo/UserController.java
+++ ssm-base/src/main/java/com/lisk/demo/UserController.java
@@ -1,5 +1,6 @@
 package com.lisk.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,9 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @RequestMapping("/hello")
     public String hello(){
-
+        User user=new User();
+        user.setUsername("ha");
+        userService.saveUser(user);
         return "hello";
     }
 
